5e8c074e6648cc190719cf0c1905f84e53b4081f
malu-sree <sreelakshmipalangattu@gmail.com>
2025-03-04 21:49:58 +0530

    exercise 8.4.1

---
+++ delegate/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegateExample
+{  
+
+    public delegate int Calulator(int a, int b);
+    class Program
+    {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        static int Div(int a, int b)
+        {
+            return a / b;
+        }
+        static void Main(string[] args)
+        {
+            Calulator operation;
+            operation = Add;
+            Console.WriteLine(operation(10, 20));
+
+            operation = Sub;
+            Console.WriteLine(operation(10, 20));
+
+            operation = Mul;        
+            Console.WriteLine(operation(10, 20)); 
+            
+            operation = Div; 
+            Console.WriteLine(operation(10, 20));
+
+        }
+    }
+}
